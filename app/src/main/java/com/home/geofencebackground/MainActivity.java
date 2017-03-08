0925b87e43fe608f096a223e726e9a5b44849828
Axel <Axel>
2017-03-08 18:32:33 -0500

    added alarm

--- app/src/main/java/com/home/geofencebackground/MainActivity.java
+++ app/src/main/java/com/home/geofencebackground/MainActivity.java
@@ -99,8 +99,8 @@ public class MainActivity extends AppCompatActivity {
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, 15);
-        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.HOUR_OF_DAY, 22);
+        calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
 
         if (calendar.getTimeInMillis() < System.currentTimeMillis()) {
